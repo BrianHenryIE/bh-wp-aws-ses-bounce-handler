3a6a7a4a9c2279028943a62c38add7568a06460f
Brian Henry <BrianHenryIE@gmail.com>
2022-06-29 17:27:59 -0700

    Remove misleading comments

--- src/API/interface-api.php
+++ src/API/interface-api.php
@@ -5,19 +5,11 @@
  * @since      1.4.0
  *
  * @package   BH_WP_AWS_SES_Bounce_Handler
- * @subpackage BH_WP_AWS_SES_Bounce_Handler/API
  */
 
 namespace BrianHenryIE\AWS_SES_Bounce_Handler\API;
 
-/**
- * The settings interface.
- *
- * @since      1.4.0
- * @package    BH_WP_AWS_SES_Bounce_Handler
- * @subpackage BH_WP_AWS_SES_Bounce_Handler/API
- * @author     BrianHenryIE <BrianHenryIE@gmail.com>
- */
+
 interface API_Interface {
 
 	public function handle_bounces( $topic_arn, $headers, $body, $message ): void;
