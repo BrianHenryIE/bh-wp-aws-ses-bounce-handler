1fd3e6db35bc84813b439d49745618669c3ca15a
Brian Henry <BrianHenryIE@gmail.com>
2026-02-13 14:35:22 -0800

    Increment version

--- src/api/class-settings.php
+++ src/api/class-settings.php
@@ -131,6 +131,6 @@ class Settings implements Settings_Interface, Logger_Settings_Interface {
 	public function get_plugin_version(): string {
 		return defined( 'BH_WP_AWS_SES_BOUNCE_HANDLER_VERSION' )
 		? BH_WP_AWS_SES_BOUNCE_HANDLER_VERSION
-			: '1.7.0';
+			: '1.8.0';
 	}
 }
