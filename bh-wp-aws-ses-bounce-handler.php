39881b974c230fb52359e10fc2ba8299755e1995
Brian Henry <BrianHenryIE@gmail.com>
2026-02-12 17:18:54 -0800

    Set PHP version to 8.0

--- bh-wp-aws-ses-bounce-handler.php
+++ bh-wp-aws-ses-bounce-handler.php
@@ -11,7 +11,7 @@
  * Plugin URI:        https://github.com/BrianHenryIE/bh-wp-aws-ses-bounce-handler
  * Description:       When AWS SES sends a bounce or complaint report, users & orders are marked; Newsletter users are unsubscribed.
  * Version:           1.7.0
- * Requires PHP:      7.4
+ * Requires PHP:      8.0
  * Author:            BrianHenryIE
  * Author URI:        https://BrianHenry.ie
  * License:           GPL-2.0+
